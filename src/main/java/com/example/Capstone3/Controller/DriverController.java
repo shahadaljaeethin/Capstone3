cef945cd96b490f4439f44df33f73a2f0a43ee8d
shahadaljaeethin <shahadaljaeethin@gmail.com>
2025-12-08 22:15:52 +0300

    controllers

--- src/main/java/com/example/Capstone3/Controller/DriverController.java
+++ src/main/java/com/example/Capstone3/Controller/DriverController.java
@@ -1,4 +1,39 @@
 package com.example.Capstone3.Controller;
 
+import com.example.Capstone3.Api.ApiResponse;
+import com.example.Capstone3.Model.Driver;
+import com.example.Capstone3.Service.DriverService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/driver")
+@RequiredArgsConstructor
 public class DriverController {
+    private final DriverService driverService;
+
+    @PostMapping("/add")
+    public ResponseEntity<?> addDriver(@RequestBody @Valid Driver driver) {
+        driverService.addDriver(driver);
+        return ResponseEntity.status(200).body(new ApiResponse("Driver added"));
+    }
+
+    @GetMapping("/get")
+    public ResponseEntity<?> getAllDrivers() {return ResponseEntity.status(200).body(driverService.getAll());
+                                              }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<?> updateDriver(@PathVariable Integer id, @RequestBody @Valid Driver driver) {
+        driverService.updateDriver(id, driver);
+        return ResponseEntity.status(200).body(new ApiResponse("Driver updated"));
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> deleteDriver(@PathVariable Integer id) {
+        driverService.deleteDriver(id);
+        return ResponseEntity.status(200).body(new ApiResponse("Driver removed"));
+    }
+
 }
--- src/main/java/com/example/Capstone3/Controller/EmergencyController.java
+++ src/main/java/com/example/Capstone3/Controller/EmergencyController.java
@@ -1,4 +1,40 @@
 package com.example.Capstone3.Controller;
 
+import com.example.Capstone3.Api.ApiResponse;
+import com.example.Capstone3.Model.Emergency;
+import com.example.Capstone3.Service.EmergencyService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/emergency")
+@RequiredArgsConstructor
 public class EmergencyController {
+    private final EmergencyService emergencyService;
+
+    @GetMapping("/get")
+    public ResponseEntity<?> getAll() {
+        return ResponseEntity.status(200).body(emergencyService.getAll());
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<?> addEmergency(@RequestBody @Valid Emergency emergency) {
+        emergencyService.addEmergency(emergency);
+        return ResponseEntity.status(200).body(new ApiResponse("Emergency contact added"));
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<?> updateEmergency(@PathVariable Integer id, @RequestBody @Valid Emergency emergency) {
+        emergencyService.updateEmergency(id, emergency);
+        return ResponseEntity.status(200).body(new ApiResponse("Emergency contact updated"));
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> deleteEmergency(@PathVariable Integer id) {
+        emergencyService.deleteEmergency(id);
+        return ResponseEntity.status(200).body(new ApiResponse("Emergency contact deleted"));
+    }
+
 }
