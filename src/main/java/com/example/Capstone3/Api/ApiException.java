b018c4dc3261816afdd0b6aa8a02e17f2c77fc06
shahadaljaeethin <shahadaljaeethin@gmail.com>
2025-12-08 18:54:21 +0300

    adding files

--- src/main/java/com/example/Capstone3/Api/ApiException.java
+++ src/main/java/com/example/Capstone3/Api/ApiException.java
@@ -1,4 +1,6 @@
 package com.example.Capstone3.Api;
 
-public class ApiException {
-}
+public class ApiException extends RuntimeException{
+    public ApiException(String message){
+        super(message);
+    }}
